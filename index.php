b7c49a2a22cd3f4a8009365f364c0a09cf691e78
0918nobita <nobita.0918@gmail.com>
2016-07-25 21:02:50 +0900

    Fix Typo

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
 namespace Pretest;
 
 require_once __DIR__ . '/config.php';
+require_once __DIR__ . '/config-secret.php';
 require_once __DIR__ . '/vendor/autoload.php';
 
-$dispatcher = new \Pretest\Dispatcher();
+$dispatcher = new Dispatcher();
 $dispatcher->dispatch();
