8ceb744b865583c12685407e512ea49c4b7f980d
0918nobita <nobita.0918@gmail.com>
2016-07-26 19:15:00 +0900

    PretestモデルにgetWordsメソッドを追加 プレテストを開始する際にデータベースから単語一覧を連想配列の形で取得する

--- models/PretestModel.php
+++ models/PretestModel.php
@@ -13,11 +13,24 @@ class PretestModel extends Model
 {
     public $max;
 
+    const NUM = 0; // 番号順
+    const RND = 1; // ランダム
+
     public function __construct()
     {
         parent::__construct();
         $stmt = $this->db->query("SELECT MAX(id) as id_max FROM words");
         $result = $stmt->fetch(\PDO::FETCH_ASSOC);
         $this->max = $result["id_max"];
+    }
+    
+    public function getWords($first, $last, $order) {
+        $stmt = $this->db->prepare("SELECT * FROM words WHERE id >= :first AND id < :last");
+        $stmt->bindValue(":first", $first);
+        $stmt->bindValue(":last", $last);
+        $stmt->execute();
+        $result = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+        if ($order == PretestModel::RND) shuffle($result);
+        return $result;
     }
 }
