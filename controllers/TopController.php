c57580883bec097d290f9a44b2e96ba0fbce28e5
0918nobita <nobita.0918@gmail.com>
2016-07-26 09:29:40 +0900

    Improve readability

--- controllers/TopController.php
+++ controllers/TopController.php
@@ -1,6 +1,14 @@
 <?php
 namespace Pretest\Controllers;
 
+/**
+ * TopController クラス
+ * トップページを表示する際に呼び出されるコントローラで、
+ * テンプレートファイルとスタイルシートファイルを指定するフィールドを上書きする。
+ * @access public
+ * @author 0918nobita
+ * @package Pretest\Controllers
+ */
 class TopController extends Controller
 {
     protected $view = 'top.html.twig';
