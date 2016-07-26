8f16833a6d7100dbcbacab65a64df00b7ee35169
0918nobita <nobita.0918@gmail.com>
2016-07-26 12:24:33 +0900

    Validatorクラスを追加した ビジネスロジックに直接入力値を渡すのではなく、この入力値検証クラスを介して検証済みの単なる値にした上で渡すようにした

--- controllers/PretestController.php
+++ controllers/PretestController.php
@@ -1,6 +1,8 @@
 <?php
 namespace Pretest\Controllers;
 
+use Pretest\Models;
+
 /**
  * PretestController クラス
  * プレテストページを表示する際に呼び出されるコントローラで、
@@ -10,8 +12,23 @@ namespace Pretest\Controllers;
  * @package Pretest\Controllers
  * @todo データベースと英単語データを受送信するPretestModelクラスから受け取ったデータをビューに渡す処理が実装できていない
  */
+
 class PretestController extends Controller
 {
     protected $view = 'pretest.html.twig';
     protected $stylesheetPath = "./views/pretest.css";
+
+    protected $validator; // 入力値検証クラスのインスタンスが代入される
+
+    public function __construct()
+    {
+        $this->validator = new Models\Validator();
+        $this->model = new Models\PretestModel();
+        parent::__construct();
+    }
+
+    public function display()
+    {
+        parent::display();
+    }
 }
---
+++ models/Validator.php
@@ -0,0 +1,15 @@
+<?php
+namespace Pretest\Models;
+
+/**
+ * Validator クラス
+ * 入力値検証クラス。コントローラから渡された入力値が条件を満たしていれば単なる値として返すメソッドを実装する。
+ * @access public
+ * @author 0918nobita
+ * @package Pretest\Models
+ * @todo 入力値を検証し条件を満たしていれば単なる値として返すメソッドを実装できていない
+ */
+
+class Validator
+{
+}
