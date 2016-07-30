5e28ef7c84f4f43592b86ac9d06dfc331507d2a4
0918nobita <nobita.0918@gmail.com>
2016-07-30 23:28:28 +0900

    Improve readability

--- controllers/ErrorController.php
+++ controllers/ErrorController.php
@@ -1,6 +1,14 @@
 <?php
 namespace Pretest\Controllers;
 
+/**
+ * ErrorController クラス
+ * エラー画面を表示する
+ * @access public
+ * @author 0918nobita
+ * @package Pretest\Controllers
+ */
+
 class ErrorController extends Controller
 {
     protected $view = "error.html.twig";
--- models/Validator.php
+++ models/Validator.php
@@ -3,7 +3,7 @@ namespace Pretest\Models;
 
 /**
  * Validator クラス
- * 入力値検証クラス。コントローラから渡された入力値が条件を満たしていれば単なる値として返すメソッドを実装する。
+ * 入力値検証を行い、不正なら場合によってはエラーページに飛ばす
  * @access public
  * @author 0918nobita
  * @package Pretest\Models
