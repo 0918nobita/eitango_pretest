7ee3b89787f8ab92e8f3dd0ed6921b4c93ed6379
0918nobita <nobita.0918@gmail.com>
2016-08-15 12:02:52 +0900

    ページリンクが表示されるようにした Paging::pagingInitで分割して表示したい項目の総数と1ページに表示する件数を設定しておき、Model::assignにPaging::addPageLinkの戻り値を渡すことでページリンクが表示されるようになる。

--- controllers/ReviewController.php
+++ controllers/ReviewController.php
@@ -7,4 +7,15 @@ class ReviewController extends Controller
     use Models\Paging;
 
     protected $view = 'review.html.twig';
+
+    public function display()
+    {
+        $this->pagingInit(count($_SESSION['incorrect']) + count($_SESSION['selected']), 50);
+        $this->assign(array(
+            'page_link' => $this->addPageLink(
+                (isset($_GET['page'])) ? $_GET['page'] : 1
+            )
+        ));
+        parent::display();
+    }
 }
--- models/Paging.php
+++ models/Paging.php
@@ -3,4 +3,38 @@ namespace Pretest\Models;
 
 trait Paging
 {
+    private $page;
+    private $offset;
+    private $total;
+    private $totalPages;
+    private $commentsPerPage;
+
+    public function pagingInit($total, $commentsPerPage)
+    {
+        $this->total = $total;
+        $this->commentsPerPage = $commentsPerPage;
+        $this->totalPages = ceil($this->total / $this->commentsPerPage);
+    }
+
+    public function addPageLink($page)
+    {
+        if (ctype_digit($page)) {
+            $this->page = (int) $page;
+        } else if ($page > $this->totalPages) {
+            $this->page = 1;
+        } else {
+            $this->page = 1;
+        }
+        $this->offset = $this->commentsPerPage * ($this->page - 1);
+        $html = "<p>";
+        for ($i = 1; $i <= $this->totalPages; $i++) {
+            if ($this->page == $i) {
+                $html .= "<strong>${i}</strong> ";
+            } else {
+                $html .= "<a href='?page=${i}'>${i}</a>";
+            }
+        }
+        $html .= "</p>";
+        return $html;
+    }
 }
