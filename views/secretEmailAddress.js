8cc3e3c8dc548c7a70fbc8081f61739efbc88b98
0918nobita <nobita.0918@gmail.com>
2016-08-16 13:05:55 +0900

    スパムを防止できるようにメールアドレスを難読化した

---
+++ views/secretEmailAddress.js
@@ -0,0 +1,5 @@
+function secretEmailAddress() {
+    var ma = String.fromCharCode(110, 111, 98, 105, 116, 97, 46, 48, 57, 49, 56, 64, 103, 109, 97, 105, 108, 46, 99, 111, 109);
+    var mt = String.fromCharCode(109,97,105,108,116,111,58);
+    document.write('<a href="'+ mt + ma + '">こちら</a>');
+}
