e36dea05481d27d462b282106c81720b77c4870f
0918nobita <nobita.0918@gmail.com>
2016-08-16 13:45:05 +0900

    復習ページで問題番号だけでなく品詞、綴り、意味も表示されるようにした 数値配列で指定した英単語の情報をまとめてwordsテーブルから取得して返すgetSelectedWordsメソッドを追加した。 今後はここにチェックボックスと削除ボタンを追加してユーザーが管理できるようにしていく。

--- models/ReviewModel.php
+++ models/ReviewModel.php
@@ -3,4 +3,17 @@ namespace Pretest\Models;
 
 class ReviewModel extends Model
 {
+    public function getSelectedWords($array) {
+        if (count($array) == 0) return array();
+        try {
+            $inClause = substr(str_repeat(',?', count($array)), 1);
+            $stmt = $this->db->prepare(sprintf('SELECT * FROM words WHERE id in (%s)', $inClause));
+            $stmt->execute($array);
+            $result = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+        } catch(\PDOException $e) {
+            echo $e->getMessage();
+            die();
+        }
+        return $result;
+    }
 }
