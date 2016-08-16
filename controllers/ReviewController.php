c316e5e5cea206b397ca5fcd1a9687b3eb6c94e3
0918nobita <nobita.0918@gmail.com>
2016-08-16 12:33:16 +0900

    選択した問題と間違った問題がセッション保存できない不具合を修正した array_mergeで未使用の添え字を指定されたセッションと別の配列を結合しようとしていたのが原因だった

--- controllers/ReviewController.php
+++ controllers/ReviewController.php
@@ -19,19 +19,19 @@ class ReviewController extends Controller
         if (isset($_GET['mode']) && ($_GET['mode'] == "incorrect")) {
             $this->assign(array(
                 'mode' => 'incorrect',
-                'incorrect' => $_SESSION['incorrect'],
+                'incorrect' => (isset($_SESSION['incorrect'])) ? $_SESSION['incorrect'] : array(),
                 'page_link_incorrect' => $this->addPageLink(
                     (isset($_GET['page'])) ? $_GET['page'] : 1,
-                    count($_SESSION['incorrect']), 50
+                    (isset($_SESSION['incorrect'])) ? count($_SESSION['incorrect']) : 0, 50
                 )
             ));
         } else {
             $this->assign(array(
                 'mode' => 'selected',
-                'selected' => $_SESSION['selected'],
+                'selected' => (isset($_SESSION['selected'])) ? $_SESSION['selected'] : array(),
                 'page_link_selected' => $this->addPageLink(
                     (isset($_GET['page'])) ? $_GET['page'] : 1,
-                    count($_SESSION['selected']), 50
+                    (isset($_SESSION['selected'])) ? count($_SESSION['selected']) : 0, 50
                 )
             ));
         }
--- models/Session.php
+++ models/Session.php
@@ -16,10 +16,16 @@ class Session
     }
 
     public function saveIncorrectProblems() {
-        $this->set("incorrect", array_merge($_SESSION['incorrect'], json_decode($_POST["incorrect"])));
+        $this->set('incorrect', array_values(array_unique(array_merge(
+            json_decode($_POST['incorrect']),
+            (isset($_SESSION['incorrect'])) ? $_SESSION['incorrect'] : array()
+        ))));
     }
 
     public function saveSelectedProblems() {
-        $this->set("selected", array_merge($_SESSION['selected'], json_decode($_POST["selected"])));
+        $this->set('selected', array_values(array_unique(array_merge(
+            json_decode($_POST['selected']),
+            (isset($_SESSION['selected'])) ? $_SESSION['selected'] : array()
+        ))));
     }
 }
